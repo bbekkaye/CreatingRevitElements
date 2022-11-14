8cd76dc993513ba19dc0a2a5fab2da1e2757380c
Bachir Bekkaye <bachirbk@gmail.com>
2022-11-14 06:41:14 -0800

    Create wall, pipes

--- CreatingRevitElements/Command.cs
+++ CreatingRevitElements/Command.cs
@@ -7,7 +7,10 @@ using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-
+using  Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
 #endregion
 
 namespace CreatingRevitElements
@@ -16,43 +19,120 @@ namespace CreatingRevitElements
     public class Command : IExternalCommand
     {
         public Result Execute(
-          ExternalCommandData commandData,
-          ref string message,
-          ElementSet elements)
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
         {
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            // Access current selection
+            // public IList<Element> PickElementsByRectangle(string statusPrompt)
+            // Use the rectangle picking tool to identify model elements to select.
 
-            Selection sel = uidoc.Selection;
+            IList<Element> pickedElements = uidoc.Selection.PickElementsByRectangle("Select by rectangle");
+            List<CurveElement> curveElement = new List<CurveElement>();
 
-            // Retrieve elements from database
+            // Collect Ids of all picked elements
+            IList<ElementId> idsToSelect = new List<ElementId>(pickedElements.Count);
 
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
+            WallType curWallType = GetWallTypeByName(doc, @"Generic - 8""");
+            Level curLevel = GetLevelByName(doc, "Level 1");
 
-            // Filtered element collector is iterable
-
-            foreach (Element e in col)
-            {
-                Debug.Print(e.Name);
-            }
-
-            // Modify document within a transaction
+            MEPSystemType curSystemType = GetSystemTypeByName(doc, "Domestic Hot Water");
+            PipeType curPipeType = GetPipeTypeByName(doc, "Default");
 
             using (Transaction tx = new Transaction(doc))
             {
-                tx.Start("Transaction Name");
+                tx.Start("Create Wall");
+
+                foreach (Element element in pickedElements)
+                {
+                    if (!(element is CurveElement)) continue;
+                    CurveElement curve = (CurveElement)element;
+                    CurveElement curve2 = (CurveElement)curve;
+
+                    curveElement.Add(curve);
+
+                    GraphicsStyle curGS = curve.LineStyle as GraphicsStyle;
+                    Curve curCurve = curve.GeometryCurve;
+                    XYZ startPoint = curCurve.GetEndPoint(0);
+                    XYZ endPoint = curCurve.GetEndPoint(1);
+
+                    //Wall newWall = Wall.Create(doc, curCurve, curWallType.Id, curLevel.Id, 15, 0, false, false);
+                    Pipe newPipe = Pipe.Create(doc, curSystemType.Id, curPipeType.Id, curLevel.Id, startPoint,
+                        endPoint);
+
+                    Debug.WriteLine(curGS.Name);
+                }
+
                 tx.Commit();
             }
 
+            // Update the current selection
+            TaskDialog.Show("Revit", $"{curveElement.Count} Elements Selected.");
             return Result.Succeeded;
+        }
+
+        private WallType GetWallTypeByName(Document doc, string wallTypeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(WallType));
+
+            foreach (Element curElement in collector)
+            {
+                WallType wallType = curElement as WallType;
+                if (wallType.Name == wallTypeName)
+                    return wallType;
+            }
+
+            return null;
+        }
+
+        private Level GetLevelByName(Document doc, string levelName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(Level));
+
+            foreach (Element curElement in collector)
+            {
+                Level level = curElement as Level;
+                if (level.Name == levelName)
+                    return level;
+            }
+
+            return null;
+        }
+
+        private MEPSystemType GetSystemTypeByName(Document doc, string typeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(MEPSystemType));
+
+            foreach (Element curElement in collector)
+            {
+                MEPSystemType curType = curElement as MEPSystemType;
+                if (curType.Name == typeName)
+                    return curType;
+            }
+
+            return null;
+        }
+
+        private PipeType GetPipeTypeByName(Document doc, string typeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(PipeType));
+
+            foreach (Element curElement in collector)
+            {
+                PipeType curType = curElement as PipeType;
+                if (curType.Name == typeName)
+                    return curType;
+            }
+
+            return null;
         }
     }
 }
